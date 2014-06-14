9291f49660660fda4445a7d9e6ddf3d9a81bf5d8
James Brennan <james@jamesbrennan.ca>
2014-06-13 22:13:03 -0700

    Start EC2 search/filtering

--- lib/skipper/cli.rb
+++ lib/skipper/cli.rb
@@ -6,13 +6,16 @@
     default_task :run_commands
 
     desc 'run_commands', 'Run a command on remote servers'
-    method_option :servers,  type: :array
-    method_option :filter,   type: :hash
-    method_option :user,     type: :string
-    method_option :run_in,   type: :string
-    method_option :wait,     type: :string
-    method_option :limit,    type: :string
-    method_option :output,   type: :boolean, default: true
+    method_option :servers,    type: :array
+
+    method_option :region,     type: :string,   default: 'us-east-1'
+    method_option :tags,       type: :hash
+
+    method_option :user,       type: :string
+    method_option :run_in,     type: :string
+    method_option :wait,       type: :string
+    method_option :limit,      type: :string
+    method_option :output,     type: :boolean,  default: true
     def run_commands
       Skipper::Banner.print
       Skipper::Repl.new(options, self).run
